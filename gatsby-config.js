a68829244a1bd8c09f5766a351497d67a3be0af1
Dimitris Kottas <info@cuboctaedro.eu>
2019-09-11 15:18:53 +0300

    netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Cuboctaedro`,
+    description: `Portfolio and blog of Dimitris Kottas, web developer and graphic designer.`,
+    author: `@cuboctaedro`,
   },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+
+        `gatsby-plugin-netlify-cms`,
+        
     {
       resolve: `gatsby-source-filesystem`,
       options: {
